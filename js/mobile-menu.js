77fd1a6461df28a0fb6dcbdcb972be55d60eae54
Min-jyo <minjyo95@gmail.com>
2019-09-26 16:16:05 +0900

    반응형 웹 공부 5

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,41 +1,49 @@
 var viewport = window.matchMedia('(max-width: 999px)');
 var items = $('.menu-item');
-
-
 $(window).resize(function(){
   location.reload();
-})
-
-if (viewport.matches) {
+});
+if(viewport.matches){
   var nav = $('.navigation');
   var btn = $('.btn-menubar');
+  var menu = $('.menu');
   var lists = $('.menu-list');
-   
+
   items.addClass('icon-plus');
-  items.attr('role','button');
-  items.attr('aria-haspopup','true');
+  items.attr('role', 'button');
+  items.attr('aria-haspopup', 'true');
   items.attr('aria-expanded', 'false');
   items.attr('aria-pressed', 'false');
-  btn.on('click', function (e) {
-  e.preventDefault();
-  nav.toggleClass('nav-act');
-  if(nav.hasClass('nav-act')){
-    btn.attr('aria-label', '메인메뉴 닫기');
-  }else{
-    btn.attr('aria-label', '메인메뉴 열기');
-  }
-});
+  btn.on('click', function(e){
+    e.preventDefault();
+    nav.toggleClass('nav-act');
+    if(nav.hasClass('nav-act')){
+      btn.attr('aria-label', '메인메뉴 닫기');
+    }else{
+      btn.attr('aria-label', '메인메뉴 열기');
+    }
+  });
 
-items.on('click', function(e){
-  lists.removeClass('menu-act');
-  $(this).parent().addClass('menu-act');
-});
+  items.on('click', function(e){
+    lists.removeClass('menu-act');
+    $(this).parent().addClass('menu-act');
+    if(lists.hasClass('menu-act')){
+      items.removeClass('icon-minus').addClass('icon-plus');
+      items.attr('aria-expanded', 'false').attr('aria-pressed', 'false')
+      $(this).removeClass('icon-plus').addClass('icon-minus');
+      $(this).attr('aria-expanded', 'true').attr('aria-pressed', 'true');
+    }
+  });
 
-} else {
-items.attr('role', 'presentation');
+  menu.on('focusin', function(){
+    nav.addClass('nav-act');
+  });
+  menu.on('focusout', function(){
+    nav.removeClass('nav-act');
+  });
+
+}else{
+  var bar = $('.bar');
+  items.attr('role', 'presentation');
+  bar.attr('aria-hidden', 'true');
 }
-
-
-
-
-
